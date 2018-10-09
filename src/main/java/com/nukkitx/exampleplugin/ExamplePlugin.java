e8b78237eb4e2bad611a7967e7154d0ae7a91e44
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-10-09 13:14:08 +0100

    Rewrite.

---
+++ src/main/java/com/nukkitx/exampleplugin/ExamplePlugin.java
@@ -0,0 +1,46 @@
+package com.nukkitx.exampleplugin;
+
+import com.nukkitx.api.Server;
+import com.nukkitx.api.event.Listener;
+import com.nukkitx.api.event.player.PlayerJoinEvent;
+import com.nukkitx.api.event.server.ServerInitializationEvent;
+import com.nukkitx.api.event.server.ServerShutdownEvent;
+import com.nukkitx.api.message.TextFormat;
+import com.nukkitx.api.message.TipMessage;
+import com.nukkitx.api.plugin.Plugin;
+import com.nukkitx.api.plugin.PluginDescription;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import java.nio.file.Path;
+
+@Plugin(id = "ExamplePlugin", authors = {"NukkitX Team"}, version = "1.0.0")
+public class ExamplePlugin {
+    private final Logger logger;
+    private final PluginDescription description;
+    private final Path folder;
+    private final Server server;
+
+    @Inject
+    private ExamplePlugin(Logger logger, PluginDescription description, Path folder, Server server) {
+        this.logger = logger;
+        this.description = description;
+        this.folder = folder;
+        this.server = server;
+    }
+
+    @Listener
+    public void onInitialization(ServerInitializationEvent event) {
+        logger.info(TextFormat.DARK_GREEN + description.getId() + " initialization!");
+    }
+
+    @Listener
+    public void onShutdown(ServerShutdownEvent event) {
+        logger.info(TextFormat.DARK_RED + description.getId() + " shutting down!");
+    }
+
+    @Listener
+    public void onJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(new TipMessage("Welcome to the test server! This is experimental server software so there may be bugs."));
+    }
+}
